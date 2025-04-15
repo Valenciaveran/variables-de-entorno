5e951aa26079fc6ee210295f266cc5219241f789
valenciaveran <valenciaveran@gmail.com>
2025-04-15 17:13:41 -0400

    search engine

--- principal.py
+++ principal.py
@@ -11,11 +11,19 @@ logging.basicConfig (
 def load_env_variables() -> Optional[Dict[str, str]]:
     load_dotenv()
     api_key = os.getenv("API_KEY")
+    search_engine_id = os.getenv("SEARCH_ENGINE_ID")
+   
+    if not api_key or not search_engine_id:
+        logging.error("api key o search engine id no estan cargadas")
+        return None
     logging.info("api key cargada correctamente")
-    print(f"tu api key {api_key}")
-
+    return {
+        'api_key' : api_key,
+        'search_engine_id' : search_engine_id
+    }
+    
 def main():
     vars = load_env_variables()
-
+    logging.info(f"api key cargada correctamente{vars}")
 if __name__ == "__main__":
     main()
