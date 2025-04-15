ad7cddff83a82d392b86a3db430b600f44f65f77
valenciaveran <valenciaveran@gmail.com>
2025-04-15 16:54:00 -0400

    logging agregado

--- principal.py
+++ principal.py
@@ -1,10 +1,17 @@
 from dotenv import load_dotenv
 import os
+import logging
 from typing import List, Dict, Optional
+
+ # configurar logging
+logging.basicConfig (
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def load_env_variables() -> Optional[Dict[str, str]]:
     load_dotenv()
     api_key = os.getenv("API_KEY")
+    logging.info("api key cargada correctamente")
     print(f"tu api key {api_key}")
 
 def main():
